b5753f013143579345a49eb884e879040132ca12
komen <g.komen@carepay.co.ke>
2019-03-25 20:39:56 +0300

    create a isPrime predicate

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         test();
+
     }
 
     private static void test() {
         // Test prime number function
-        System.out.println("Actual true, Got: " + PrimeAndMemoize.isPrime(2));
-        System.out.println("Actual false, Got: " + PrimeAndMemoize.isPrime(-1));
-        System.out.println("Actual true, Got: " + PrimeAndMemoize.isPrime(101));
-        System.out.println("Actual false, Got: " + PrimeAndMemoize.isPrime(999999998));
+        System.out.println("Actual true, Got: " + PrimeAndMemoize.isPrimePredicate().test(2));
+        System.out.println("Actual false, Got: " + PrimeAndMemoize.isPrimePredicate().test(-1));
+        System.out.println("Actual true, Got: " + PrimeAndMemoize.isPrimePredicate().test(101));
+        System.out.println("Actual false, Got: " + PrimeAndMemoize.isPrimePredicate().test(999999998));
     }
 }
--- src/PrimeAndMemoize.java
+++ src/PrimeAndMemoize.java
@@ -1,5 +1,13 @@
+import java.util.function.Predicate;
+
 public class PrimeAndMemoize {
-    public static boolean isPrime(int number) {
+
+
+    public static Predicate<Integer> isPrimePredicate() {
+        return PrimeAndMemoize::isPrime;
+    }
+
+    private static boolean isPrime(int number) {
         if (number <= 1) return false;
 
         for (int i = 2; i < Math.sqrt(number); i++) {
