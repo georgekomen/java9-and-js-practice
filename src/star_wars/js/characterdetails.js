946169b65ffafe1e8bc6c68d764ad349bc6ef0d3
komen <g.komen@carepay.co.ke>
2019-03-27 23:52:51 +0300

    move template generating code to separate functions

--- src/star_wars/js/characterdetails.js
+++ src/star_wars/js/characterdetails.js
@@ -1,91 +1,95 @@
+var request = new XMLHttpRequest();
 var urlParams = new URLSearchParams(window.location.search);
 var detailsUrl = urlParams.get('detailsUrl');
 var data;
-var request = new XMLHttpRequest();
+
 request.open('GET', detailsUrl, true);
 request.onload = function() {
     // Begin accessing JSON data here
     data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-        var html = "<h2>" + data.name;
-        if (isFavourite(data.url)) {
-            html += "<i style='margin-left: 1em;  color: red' onclick='makeFavourite(this.id)' class='fas fa-heart'" + "id=" + data.url + "></i>";
-        } else {
-            html += "<i style='margin-left: 1em; color: grey' onclick='makeFavourite(this.id)' class='fas fa-heart'" + "id=" + data.url + "></i>";
-        }
-        if (isFavourite(data.url)) {
-            html += "<i style='margin-left: 1em; color: grey' onclick='removeFromFavourite(this.id)' class='fas fa-trash-alt'" + "id=" + data.url + "></i>";
-        }
-        html += "</h2>";
-
-        html += "<span>";
-        html += "<h3>" + "Bio information" + "</h3>";
-        html += "Height: " + data.height;
-        html += "<br>";
-        html += "Mass: " + data.mass;
-        html += "<br>";
-        html += "Hair color: " + data.hair_color;
-        html += "<br>";
-        html += "Skin color: " + data.skin_color;
-        html += "<br>";
-        html += "Eye color: " + data.eye_color;
-        html += "<br>";
-        html += "Birth year: " + data.birth_year;
-        html += "<br>";
-        html += "Gender: " + data.gender;
-        html += "<br>";
-        html += "Home world: " + data.home_world;
-        html += "<br>";
-        html += "Created: " + data.created;
-        html += "<br>";
-        html += "url: " + data.url;
-        html += "<br>";
-        html += "</span>";
-
-        if (data.films.length > 0) {
-            html += "<h3>" + "Films" + "</h3>";
-            html += "<ul>";
-            for (var i = 0; i < data.films.length; i++) {
-                html += "<li>" + data.films[i] + "</li>";
-            }
-            html += "</ul>";
-        }
-
-        if (data.species.length > 0) {
-            html += "<h3>" + "Species" + "</h3>";
-            html += "<ul>";
-            for (var i = 0; i < data.species.length; i++) {
-                html += "<li>" + data.species[i] + "</li>";
-            }
-            html += "</ul>";
-        }
-
-        if (data.vehicles.length > 0) {
-            html += "<h3>" + "Vehicles" + "</h3>";
-            html += "<ul>";
-            for (var i = 0; i < data.vehicles.length; i++) {
-                html += "<li>" + data.vehicles[i] + "</li>";
-            }
-            html += "</ul>";
-        }
-
-        if (data.starships.length > 0) {
-            html += "<h3>" + "Starships" + "</h3>";
-            html += "<ul>";
-            for (var i = 0; i < data.starships.length; i++) {
-                html += "<li>" + data.starships[i] + "</li>";
-            }
-            html += "</ul>";
-        }
-
-        document.getElementById("details_div").innerHTML = html;
-        document.getElementById('loader').style.visibility = 'hidden';
-
+        updateTemplate();
     } else {
         alert('error fetching data');
     }
 };
 request.send();
+
+function updateTemplate() {
+    var html = "<h2>" + data.name;
+    if (isFavourite(data.url)) {
+        html += "<i style='margin-left: 1em;  color: red' onclick='makeFavourite(this.id)' class='fas fa-heart'" + "id=" + data.url + "></i>";
+    } else {
+        html += "<i style='margin-left: 1em; color: grey' onclick='makeFavourite(this.id)' class='fas fa-heart'" + "id=" + data.url + "></i>";
+    }
+    if (isFavourite(data.url)) {
+        html += "<i style='margin-left: 1em; color: grey' onclick='removeFromFavourite(this.id)' class='fas fa-trash-alt'" + "id=" + data.url + "></i>";
+    }
+    html += "</h2>";
+
+    html += "<span>";
+    html += "<h3>" + "Bio information" + "</h3>";
+    html += "Height: " + data.height;
+    html += "<br>";
+    html += "Mass: " + data.mass;
+    html += "<br>";
+    html += "Hair color: " + data.hair_color;
+    html += "<br>";
+    html += "Skin color: " + data.skin_color;
+    html += "<br>";
+    html += "Eye color: " + data.eye_color;
+    html += "<br>";
+    html += "Birth year: " + data.birth_year;
+    html += "<br>";
+    html += "Gender: " + data.gender;
+    html += "<br>";
+    html += "Home world: " + data.home_world;
+    html += "<br>";
+    html += "Created: " + data.created;
+    html += "<br>";
+    html += "url: " + data.url;
+    html += "<br>";
+    html += "</span>";
+
+    if (data.films.length > 0) {
+        html += "<h3>" + "Films" + "</h3>";
+        html += "<ul>";
+        for (var i = 0; i < data.films.length; i++) {
+            html += "<li>" + data.films[i] + "</li>";
+        }
+        html += "</ul>";
+    }
+
+    if (data.species.length > 0) {
+        html += "<h3>" + "Species" + "</h3>";
+        html += "<ul>";
+        for (var i = 0; i < data.species.length; i++) {
+            html += "<li>" + data.species[i] + "</li>";
+        }
+        html += "</ul>";
+    }
+
+    if (data.vehicles.length > 0) {
+        html += "<h3>" + "Vehicles" + "</h3>";
+        html += "<ul>";
+        for (var i = 0; i < data.vehicles.length; i++) {
+            html += "<li>" + data.vehicles[i] + "</li>";
+        }
+        html += "</ul>";
+    }
+
+    if (data.starships.length > 0) {
+        html += "<h3>" + "Starships" + "</h3>";
+        html += "<ul>";
+        for (var i = 0; i < data.starships.length; i++) {
+            html += "<li>" + data.starships[i] + "</li>";
+        }
+        html += "</ul>";
+    }
+
+    document.getElementById("details_div").innerHTML = html;
+    document.getElementById('loader').style.visibility = 'hidden';
+}
 
 function makeFavourite(url) {
     var favouritesMap = JSON.parse(localStorage.getItem('favouritesMap'));
@@ -98,7 +102,7 @@ function makeFavourite(url) {
         favouritesMap.push(url);
     }
     localStorage.setItem('favouritesMap', JSON.stringify(favouritesMap));
-    document.getElementById(url).style.color = 'red';
+    updateTemplate();
 }
 
 function isFavourite(url) {
@@ -112,5 +116,5 @@ function removeFromFavourite(url) {
     var favouritesMap = JSON.parse(localStorage.getItem('favouritesMap'));
     favouritesMap.splice(favouritesMap.indexOf(url), 1);
     localStorage.setItem('favouritesMap', JSON.stringify(favouritesMap));
-    document.getElementById(url).style.color = 'grey';
+    updateTemplate();
 }
--- src/star_wars/js/characterlist.js
+++ src/star_wars/js/characterlist.js
@@ -1,37 +1,36 @@
 var request = new XMLHttpRequest();
+var data;
+
 request.open('GET', 'https://swapi.co/api/people/', true);
 request.onload = function() {
     // Begin accessing JSON data here
-    var data = JSON.parse(this.response);
+    data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-
-        var html = "<ul>";
-        for (var i = 0; i < data.results.length; i++) {
-            html += "<li onclick='gotToDetailsPage(this.id)' id=" + data.results[i].url + ">" + data.results[i].name;
-            if (isFavourite(data.results[i].url)) {
-                html += "<i style='margin-left: 1em; color: red' class='fas fa-heart'></i>";
-            } else {
-
-            }
-            html += "</li>";
-        }
-        html+="</ul>";
-        document.getElementById("character_list").innerHTML = html;
-
-        document.getElementById('loader').style.visibility = 'hidden';
-
+        updateTemplate();
     } else {
         alert('error fetching data');
     }
 };
 request.send();
 
+function updateTemplate() {
+    var html = "<ul>";
+    for (var i = 0; i < data.results.length; i++) {
+        html += "<li onclick='gotToDetailsPage(this.id)' id=" + data.results[i].url + ">" + data.results[i].name;
+        if (isFavourite(data.results[i].url)) {
+            html += "<i style='margin-left: 1em; color: red' class='fas fa-heart'></i>";
+        }
+        html += "</li>";
+    }
+    html+="</ul>";
+    document.getElementById("character_list").innerHTML = html;
+    document.getElementById('loader').style.visibility = 'hidden';
+}
 
 function gotToDetailsPage(url) {
     var querystring = '?detailsUrl=' + url;
     window.open('./details.html' + querystring, '_blank');
 }
-
 
 function isFavourite(url) {
     var favouritesMap = JSON.parse(localStorage.getItem('favouritesMap'));
